154bd45dbe14d149ef78fb20f52a5c9987e950b9
Gabriel Lima <derlima3@gmail.com>
2026-05-06 00:57:56 -0300

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import { Navbar } from './components/navbar/Navbar';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Analytics/>
       <Navbar />
       <AppRoutes />
     </BrowserRouter>
